42bfb1438ff7a68a28d32bff22655f01c8994ac9
unknown <patryk.pora@shitcoins.club>
2022-10-20 21:18:19 +0200

    simple task in gradle to create testing data

--- src/main/resources/insertInitialData.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO posts (id, title, content)
-values ( 1, 'first test post', 'content of first test post' );
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,5 +1,19 @@
-CREATE TABLE posts (
-    id INTEGER AUTO_INCREMENT PRIMARY KEY,
-    title VARCHAR(300) NOT NULL ,
-    content VARCHAR(5000) NULL
+CREATE TABLE posts
+(
+    id      INTEGER AUTO_INCREMENT PRIMARY KEY,
+    title   VARCHAR(300) NOT NULL,
+    content VARCHAR(5000) NULL,
+    created TIMESTAMP
 );
+
+CREATE TABLE comment
+(
+    id      INTEGER AUTO_INCREMENT PRIMARY KEY,
+    post_id INTEGER NOT NULL,
+    content VARCHAR(1500) NULL,
+    created TIMESTAMP
+);
+
+ALTER TABLE comment
+    ADD CONSTRAINT comment_posts_id
+        FOREIGN KEY (post_id) REFERENCES posts (id)
